0d0d170c231985af37daedcdcdb565414f60d514
Tuan Le <30828528+tuankg1028@users.noreply.github.com>
2025-01-14 20:20:36 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -94,6 +94,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href={`${basePath}/feed.xml`} />
+      <meta name="site-verification" content="6200d936e4550ae7bcc2a418bf4beeb8"/>
       <body className="bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-gray-950 dark:text-white">
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
